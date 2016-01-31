0d2aafb6ba45a061d647989cbd42f73d1160c0ac
Yevhen Khomiak <khomyak_eugene@yahoo.com>
2016-01-31 12:32:54 +0200

    Another refactoring

--- src/com/company/utils/Collections.java
+++ src/com/company/utils/Collections.java
@@ -6,13 +6,13 @@ import java.util.AbstractCollection;
  * Created by Yevgen on 17.01.2016 as a part of the project "Unit8_Homework".
  */
 public class Collections {
-    public static void printList(AbstractCollection list) {
+    public static void printList(AbstractCollection<? extends Object> list) {
         list
                 .stream()
                 .forEach(p -> Utils.printMessage(p.toString()));
     }
 
-    public static void printListAsTable(AbstractCollection list, String header, int width) {
+    public static void printListAsTable(AbstractCollection<? extends Object> list, String header, int width) {
         // Header
         Table.printTableHeader(header, width);
 
